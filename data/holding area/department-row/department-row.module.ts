6760c6b871deaf0a22c483e7a4fa212eb5a3d279
Frank Filippis <frank@filippis.com>
2024-02-21 19:32:12 -0700

    aa

--- data/holding area/department-row/department-row.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { DepartmentRowComponent } from './department-row.component';
-import { ProfileTableComponent } from '../../../test1/src/app/profile-table/profile-table.component';
-import { DepartmentItemRowComponent } from '../../../test1/src/app/department-item-row/department-item-row.component'
